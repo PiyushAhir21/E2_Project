274f678e437c38c402ee2301bc0a431983e491e8
HP <HP@SoNi.soni>
2025-03-04 18:10:16 +0530

    no changes

--- vtiger_crm_framework/src/test/java/extra/RandomNumber.java
+++ vtiger_crm_framework/src/test/java/extra/RandomNumber.java
@@ -8,6 +8,12 @@ public class RandomNumber {
 		int random = ran.nextInt(100000000);
 		System.out.println(random);
 		
+		
+		
+		String s="my name is ankush";
+		char c1=s.charAt(3);
+		System.out.println(c1);
+		
 		System.out.println((int)(Math.random()*1000));
 	}
 }
